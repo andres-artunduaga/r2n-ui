c3e155ba3e04a46543bf4d4b68a22b8cc9cf5fe6
Andres Artunduaga <andres.artunduaga@outlook.com>
2021-10-07 18:49:06 -0500

    feat: initial angular components

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,18 +1,21 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [];
 
 export const config: Config = {
   namespace: 'r2n-ui',
-  plugins: [
-    sass()
-  ],
+  plugins: [sass()],
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: '@r2n/ui',
+      directivesProxyFile: '../r2-ui-angular/src/directives/proxies.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
