1c4e59fc676e2d6175124b10c992e0896c1faf5e
Andres Artunduaga <andres.artunduaga@outlook.com>
2021-10-07 22:27:51 -0500

    feat: monorepo stencil + react + angular

---
+++ r2n-components-react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@r2n/ui';
+
+import { defineCustomElements } from '@r2n/ui/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
--- r2n-components/stencil.config.ts
+++ r2n-components/stencil.config.ts
@@ -1,16 +1,23 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
 
 const angularValueAccessorBindings: ValueAccessorConfig[] = [];
 
 export const config: Config = {
-  namespace: 'r2n-ui',
+  namespace: 'r2n-components',
   plugins: [sass()],
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@r2n/ui',
+      proxiesFile: '../r2n-components-react/src/components.ts',
+      includeDefineCustomElements: true,
+    }),
     angularOutputTarget({
       componentCorePackage: '@r2n/ui',
-      directivesProxyFile: '../r2-ui-angular/src/directives/proxies.ts',
+      directivesProxyFile: '../r2n-components-angular/src/directives/proxies.ts',
       valueAccessorConfigs: angularValueAccessorBindings,
     }),
     {
