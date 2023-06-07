d0f66feb4c26cc3a4b02a0f811cc159f7e74b4f7
Andres Artunduaga <andres.artunduaga@outlook.com>
2023-06-07 18:33:49 -0500

    adding angularoutput target

--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
